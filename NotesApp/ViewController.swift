66e8b56ea4d92cbd810ec2b7984165563125e417
Arber Basha <bashaarber@outlook.com>
2019-09-17 15:01:56 +0200

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Arber Basha on 17/09/2019.
+//  Copyright © 2019 Arber Basha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
